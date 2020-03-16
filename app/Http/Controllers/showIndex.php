2183d9f2bd96952fe50b555c26d5a35e67740f7b
Nino Chimento <saninoc84@gmail.com>
2020-03-16 14:13:01 +0100

    creo array data prova

--- app/Http/Controllers/showIndex.php
+++ app/Http/Controllers/showIndex.php
@@ -5,8 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class showIndex extends Controller
-{
+{   
+    
+    
+
     public function cd(){
-        return view("index");
+
+        $data = [
+            "cd1"=> ["nino","chimento"]
+        ];
+        return view("index",$data);
     }
 }
